806ddac8ef1004a531efff1f93a8c9a8f8980675
Carlos Eduardo Ortega Coello <20253RD027@utez.edu.mx>
2026-01-28 12:53:43 -0600

    hice un archivo llamado Siclos

---
+++ Desktop/Ciclos/Siclos.java
@@ -0,0 +1,19 @@
+public class Ciclos{
+    public static void main(String[]args){
+        //while
+        int i = 0;
+
+        //while como contador
+        while(i<=20){
+            if (1 % 2 != 0){
+                System.out.println("el valor es:" +i);
+            }
+            i++;
+            
+
+         
+        }
+
+    }
+    
+}
